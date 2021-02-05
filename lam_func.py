2982f4be270657ec0b7c776e474105f03fe9e95f
Roshan <roshan.paudel@morgan.edu>
2021-02-05 10:16:50 -0500

    create_an_example

---
+++ lam_func.py
@@ -0,0 +1,15 @@
+lamb_func1 = lambda x: 3 * x + 5
+# call the lambda dunction
+print(lamb_func1(5))
+print(lamb_func1(56))
+
+# A normal function
+def my_func(x):
+	return 3 * x + 5
+# call the function
+print(my_func(5))
+print(my_func(56))
+
+# Another example
+lamb_func2 = lambda w, x : w * x *5
+print(lamb_func2(15, 17))
